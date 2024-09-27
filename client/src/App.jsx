0900bb9888a996942bbd30a75b3c1cbd2ed91f24
Dhruv Pathak <dhruvpathak414@gmail.com>
2024-09-28 00:44:29 +0530

    New Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route ,NavLink} from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Container from "./components/Container";
-
+import About from "./components/About";
+import RatedQuestions from "./components/RatedQuestions";
 function App() {
   return (
     <>
       <Header />
-      <Container />
+     
+      <Routes>
+        <Route path="/" element={<Container />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Ratedquestions" element={<RatedQuestions />}></Route>
+      </Routes>
+
+      
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -19,7 +20,9 @@ const theme = extendTheme({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
 );
